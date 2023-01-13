55bf7d27cb7a227ac42b116504c754b1d34d6aed
Crystal Campbell <blankpage@gmail.com>
2023-01-13 16:37:44 -0600

    Fix: Linked list works

--- tech-questions-2.03.rb
+++ tech-questions-2.03.rb
@@ -1,57 +1,98 @@
 class Node
+  attr_accessor :data, :next, :previous
 
-    def initialize(data)
-        @data = data
-        @next = nil
-        @previous
-    end
+  def initialize(data)
+    @data = data
+    @next = nil
+    @previous = nil
+  end
 end
 
 class LinkedList
+  attr_accessor :head, :tail
 
+  def initialize
+    @head = nil
+    @tail = nil
+    @length = 0
+  end
 
+  def push(data)
+    node = Node.new(data)
 
-
-    def initialize
-        @head
-        @tail
-
+    if @head == nil
+      @head = node
+      @tail = node
+    else
+      @tail.next = node
+      @tail = node
     end
 
-    def push(data)
-        if @head = nil
-        @head = Node.new(data)
+    @length += 1
+  end
 
-        @head.next = nil
-        
-       else
-        @tail.next = Node.new(data)
-        
-       end
+  def print
+    current_node = @head
 
-        node = Node.new(data)
-
-    self
-
+    while current_node != nil
+      puts current_node.data
+      current_node = current_node.next
     end
-
-    
+  end
 end
 
 class DoublyLinkedList
-    def initialize
-        @head
-        @tail
+  def initialize
+    @head = nil
+    @next = nil
+    @previous = nil
+    @length = 0
+  end
 
+  def push(data)
+    node = Node.new(data)
+
+    if @head == nil
+      @head = node
+      @tail = node
+    else
+      node.previous = @tail
+      @tail.next = node
+
+      @tail = node
     end
 
-    def push(data)
+    @length += 1
+  end
 
-        node = Node.new(data)
-    self
+  def print
+    current_node = @head
 
+    while current_node != nil
+      puts current_node.data
+      current_node = current_node.next
     end
-
-
+  end
 end
 
+list = LinkedList.new
+
+list.push("go")
+
+list.push("for")
+
+list.push("it")
+
+puts list
+
+list.print
+
+dlist = DoublyLinkedList.new
+
+dlist.push("double")
+
+dlist.push("linked")
+
+dlist.push("list")
+
+dlist.print
