ff850fcb714e7e184309bddf752b518f6c7e38cf
Crystal Campbell <blankpage@gmail.com>
2023-01-13 14:56:37 -0600

    Update tech-questions-2.03.rb

--- tech-questions-2.03.rb
+++ tech-questions-2.03.rb
@@ -1,13 +1,57 @@
 class Node
 
+    def initialize(data)
+        @data = data
+        @next = nil
+        @previous
+    end
 end
 
 class LinkedList
+
+
+
+
+    def initialize
+        @head
+        @tail
+
+    end
+
+    def push(data)
+        if @head = nil
+        @head = Node.new(data)
+
+        @head.next = nil
+        
+       else
+        @tail.next = Node.new(data)
+        
+       end
+
+        node = Node.new(data)
+
+    self
+
+    end
 
     
 end
 
 class DoublyLinkedList
-    
+    def initialize
+        @head
+        @tail
+
+    end
+
+    def push(data)
+
+        node = Node.new(data)
+    self
+
+    end
+
+
 end
 
